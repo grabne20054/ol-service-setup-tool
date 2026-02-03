c5c4a885b71bcb0e9ee8a77261f94fd8c3d3661e
grabne20054 <grabne20054@gmail.com>
2026-02-03 21:33:04 +0100

    refactor setup service endpoint to use background tasks and improve response model

--- app/main.py
+++ app/main.py
@@ -1,30 +1,26 @@
-from fastapi import FastAPI
-from lib.models import company_information
-from lib.coding_information import CodingInformationHandler
-from lib.setup_service import SetupService
-from lib.config_loader import ConfigLoader
-from lib.update_rp_configs import UpdateRPConfigs
-from lib.service_status import ServiceStatus
-from lib.models.status import Status
+from fastapi import FastAPI, BackgroundTasks
+from lib.models import client_information
+from lib.init_service.setup_service import SetupService
+from lib.models.status import  StatusResponse
+from lib.init_service.setup_initated import SetupInitiated
+from uuid import uuid4
 
-from threading import Thread
-app = FastAPI()
+app = FastAPI(openapi_webhooks={})
 
-@app.post("/setup-service/")
-async def setup_service(company_info: company_information.CompanyInformation):
-    try:
-        setup_service_instance = SetupService(service_name=company_info.company_name)
-        thread = Thread(target=setup_service_instance.initialize_service)
-        thread.start()
-    except Exception as e:
-        return {"error": str(e)}
-    return {"message": f"Service setup initiated for {company_info.company_name}"}
+@app.post("/setup-service/", response_model=StatusResponse, status_code=202)
+async def setup_service(company_info: client_information.ClientInformation, background_tasks: BackgroundTasks):
+    
+    def background_setup():
+        setup_service = SetupService(
+            service_name=company_info.company_name,
+            initial_state=SetupInitiated(),
+            callback_url=company_info.callback_url
+        )
+        setup_service.run()
+    background_tasks.add_task(background_setup)
+    job_id = str(uuid4())
+    return StatusResponse(message=f"Service setup job {job_id} initiated for {company_info.company_name}", status=202)
 
-@app.get("/service-status/{service_name}")
-async def service_status(service_name: str):
-    service_status_instance = ServiceStatus(service_name=service_name)
-    status = service_status_instance.get_status()
-    return Status(id=service_name, status=status)
 
 
 
